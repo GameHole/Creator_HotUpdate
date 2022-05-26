50f486d23b5446870701fbb9df71c0be90895ff7
GameHole <741419087@qq.com>
2022-05-26 13:44:54 +0800

    [1.0.1]
    测试

--- assets/z_test/TestUpdater.ts
+++ assets/z_test/TestUpdater.ts
@@ -1,22 +1,21 @@
 
-import { _decorator, Component, Node, resources, Button } from 'cc';
-import { HotFileMgr } from '../HotUpdate/Helper/HotFileMgr';
+import { _decorator, Component, Node, Button } from 'cc';
 const { ccclass, property } = _decorator;
 
 /**
  * Predefined variables
- * Name = NewComponent
- * DateTime = Thu Apr 21 2022 14:46:31 GMT+0800 (中国标准时间)
+ * Name = TestUpdater
+ * DateTime = Sat May 07 2022 15:01:40 GMT+0800 (中国标准时间)
  * Author = qq741419087
- * FileBasename = NewComponent.ts
- * FileBasenameNoExtension = NewComponent
- * URL = db://assets/NewComponent.ts
+ * FileBasename = TestUpdater.ts
+ * FileBasenameNoExtension = TestUpdater
+ * URL = db://assets/z_test/TestUpdater.ts
  * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
  *
  */
  
-@ccclass('NewComponent')
-export class NewComponent extends Component {
+@ccclass('TestUpdater')
+export class TestUpdater extends Component {
     // [1]
     // dummy = '';
 
@@ -24,16 +23,24 @@ export class NewComponent extends Component {
     // @property
     // serializableDummy = 0;
     @property(Button)
-    btn: Button;
+    startBtn: Button;
+    @property(Button)
+    stopBtn: Button;
     start()
     {
-        let n: number = 9999999999;
-        console.log(n);
-        this.btn.node.on("click", () =>
+        this.startBtn.node.on("click", () =>
         {
-            console.log("aaa");
-            HotFileMgr.Run("test");
+            jsb.reflection.callStaticMethod("com/cocos/game/UpdaterRunner", "StartUpdater", "()V");
+
         });
+        this.stopBtn.node.on("click", () =>
+        {
+            jsb.reflection.callStaticMethod("com/cocos/game/UpdaterRunner", "StopUpdater", "()V");
+        });
+        setInterval(() =>
+        {
+            console.log("TestUpdater");
+        }, 500);
     }
 
     // update (deltaTime: number) {
